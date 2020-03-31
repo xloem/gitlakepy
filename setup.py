839085d8e34200ab6ac223d90ab6245ba1e33f21
Karl Semich <0xloem@gmail.com>
2020-03-31 19:56:25 +0000

    Beginning of Sia Skynet remote

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='gitlakepy',
       author_email='gmkarl@gmail.com',
       url='https://github.com/gmkarl/gitlakepy',
       py_modules=['gitlake'],
-      scripts=['scripts/git-annex-remote-freenet'],
-      requires=['fcp'],
+      scripts=['scripts/git-annex-remote-freenet','scripts/git-annex-remote-siaskynet'],
+      requires=['fcp', 'siaskynet'],
      )
