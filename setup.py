505d743d15b6d53a736f8f4dfe51556605be6961
Karl Semich <0xloem@gmail.com>
2020-05-08 11:50:23 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.9',
+      version='0.10',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
