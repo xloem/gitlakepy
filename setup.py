7eb43bbbea60fe0062ac1f1d05ab3ceed2937ff2
Karl Semich <0xloem@gmail.com>
2020-05-31 03:56:55 -0400

    slight mirror update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.12',
+      version='0.13',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
