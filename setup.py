49efb29af25896ba9d7e99f88ca5bd45d739605c
Karl Semich <0xloem@gmail.com>
2020-06-16 15:33:35 -0400

    fix for new python-skynet

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.15',
+      version='0.16',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
