e474eabffea88db072f57ba68f997ae08330e3d7
Karl Semich <0xloem@gmail.com>
2020-07-23 17:29:57 -0400

    fix to previous fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.20',
+      version='0.21',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
