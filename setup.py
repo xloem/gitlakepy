e0a6954e482c39c676af03085d28fb9257557b6d
Karl Semich <0xloem@gmail.com>
2020-08-02 15:02:29 -0400

    inaccessible portal fix, also small update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.21',
+      version='0.22',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
