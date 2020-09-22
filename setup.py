17549c002dad3e3e1002d64a9284b39231a9a243
Karl Semich <0xloem@gmail.com>
2020-09-22 08:25:05 -0400

    more debug output

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.22',
+      version='0.23',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
