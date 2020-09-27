13cd5c55259fe89da1dab125e17381c64bf29513
Karl Semich <0xloem@gmail.com>
2020-09-27 01:53:18 -0400

    mirror status changes i bumped into

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlakepy',
-      version='0.24',
+      version='0.25',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='gmkarl@gmail.com',
