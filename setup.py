23522a9a0c8e17ba0bcad51060cf94d3f68f444f
Karl Semich <0xloem@gmail.com>
2020-10-01 15:21:12 -0400

    now generates new private key if none provided

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.29',
+      version='0.30',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
