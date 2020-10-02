18b0c364da2f8a70d157e5efa3318ad2c8d9b09b
Karl Semich <0xloem@gmail.com>
2020-10-02 12:02:30 -0400

    specify length when doing streamed uploading

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.31',
+      version='0.32',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
