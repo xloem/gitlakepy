37abe93e26acae269cc98f599a4997ef01b3abac
Karl Semich <0xloem@gmail.com>
2020-10-07 11:35:52 -0400

    claim mirror urls

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.34',
+      version='0.35',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
