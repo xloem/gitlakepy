11e7fe1fc02e07601e83271ea9beaf597c3d0f93
xloem <0xloem@gmail.com>
2022-02-22 05:00:20 -0500

    -

--- scripts/w3_wip.py
+++ scripts/w3_wip.py
@@ -1,6 +1,18 @@
+
+'''more effective to make boilerplate for git-annex chunking than to make this library file
+   can use npm @web3.storage/w3 subprocess'''
+
+MAX_PUT_RETRIES = 5
 DEFAULT_API = 'https://api.web3.storage'
 
 class Client:
-    def __Init__(token, API = DEFAULT_API):
+    def __Init__(token, endpoint = DEFAULT_API):
         self.token = token
-        self.API = API
+        self.endpoint = API
+    @property
+    def headers(self):
+        return {
+            'Authorization': 'Bearer ' + self.token,
+            'X-Client': 'gitlakepy',
+        }
+    def put(self, files : list, onRootCidReady = None, onStoredChunk = None, maxRetries = MAX_PUT_RETRIES, wrapWithDirectory = True, name = None):
