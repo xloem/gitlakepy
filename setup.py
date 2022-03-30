5a05975e690bdbd376fad13c644f33cfe7b88336
John Doe <john.doe@example.com>
2022-03-30 12:02:41 -0400

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.43',
+      version='0.44',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
