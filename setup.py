4a7068c8e70ddc32ec363e04b583c2946aed2456
John Doe <john.doe@example.com>
2022-04-14 12:22:54 -0400

    logic error: fix priority of subchunking and combining

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.44',
+      version='0.45',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
