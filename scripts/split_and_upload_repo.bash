3f9844df5d3389f70ad373d2f61b15cd53b06850
John Doe <john.doe@example.com>
2022-05-14 13:02:01 -0400

    -

--- scripts/split_and_upload_repo.bash
+++ scripts/split_and_upload_repo.bash
@@ -4,7 +4,7 @@ MAXFILECOUNT=256
 # unpack all packfiles
 mv objects/pack/* . 2>/dev/null &&
 for pack in *.pack; do
-	git unpack-objects < "$pack" && rm "$pack"
+	git unpack-objects < "$pack" && rm -vf "$pack"
 done
 
 # make small object stores, can use dirsplit
@@ -30,7 +30,7 @@ do
 		ALTERNATES_DEPTH=1
 	fi
 	arkb deploy "$dir" -v --no-colors --concurrency 256 --auto-confirm --use-bundler https://node2.bundlr.network --timeout $((60*60*1000)) --tag-name Type --tag-value git-object-store | tee ../"$dir".arkb.log || exit -1
-	txid="$(tail -n 1 "$dir".arkb.log | cut -d '/' -f 4)"
+	txid="$(tail -n 1 ../"$dir".arkb.log | cut -d '/' -f 4)"
 	mv "$dir" "../$txid" || exit -1
 	echo "../../$txid" >> alternates-$((ALTERNATES_DEPTH))
 	{ cd ../$txid; find -type f; } | { cd objects; xargs rm -vrf; }
