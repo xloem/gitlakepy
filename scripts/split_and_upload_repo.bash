016ee84ca5220374d027f786688fcf7c97f08d25
John Doe <john.doe@example.com>
2022-05-15 06:40:09 -0400

    -

--- scripts/split_and_upload_repo.bash
+++ scripts/split_and_upload_repo.bash
@@ -12,12 +12,12 @@ prefix="$(date --iso=seconds)"
 dirsplit -L --accuracy 1 --prefix "alt-$prefix-" --blksize $((MAXFILESIZE)) --size=$(((MAXFILECOUNT-1) * MAXFILESIZE)) objects
 
 # upload each dir, accumulating the upload into an alternates file
-LONGEST_TXID=0
+LONGEST_TXID=43
 ALTERNATES_DEPTH=0 # it is not actually a tree, as i am confused.
 touch alternates-$ALTERNATES_DEPTH
 for dir in alt-${prefix}-*
 do
-	NEXT_SIZE=$LONGEST_TXID
+	NEXT_SIZE=$((LONGEST_TXID + 1))
 	for alternates_file in alternates-*
 	do
 		NEXT_SIZE=$((NEXT_SIZE + $(stat -c %s "$alternates_file")))
@@ -31,6 +31,11 @@ do
 	fi
 	if ! arkb deploy "$dir" -v --no-colors --concurrency 256 --auto-confirm --use-bundler https://node2.bundlr.network --timeout $((60*60*1000)) --tag-name Type --tag-value git-object-store | tee ../"$dir".arkb.log; then exit -1; fi
 	txid="$(tail -n 1 ../"$dir".arkb.log | cut -d '/' -f 4)"
+	TXID_LEN=$(($(echo -n "$txid" | wc -c)))
+	if (( TXID_LEN != LONGEST_TXID ))
+	then
+		exit -1
+	fi
 	if ! curl -v https://arweave.net/"$txid"; then exit -1; fi
 	if ! mv "$dir" "../$txid"; then exit -1; fi
 	rm ../"$txid"/*/manifest.arkb
@@ -38,12 +43,6 @@ do
 	{ cd ../$txid; find -type f; } | { cd objects; xargs rm -vrf; }
 	sed 's!^!../!' alternates-$((ALTERNATES_DEPTH)) > objects/info/alternates
 	ALTERNATES_DEPTH=0
-
-	TXID_LEN=$(($(echo "$txid" | wc -c)))
-	if (( TXID_LEN > LONGEST_TXID ))
-	then
-		LONGEST_TXID="$TXID_LEN"
-	fi
 done
 
 cat alternates-* > objects/info/alternates
