39207433580cfaf17e0b0b5be6647284b56c8604
John Doe <johndoe@example.com>
2022-11-01 08:46:29 -0400

    fixed bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.52',
+      version='0.53',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
