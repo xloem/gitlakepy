d7b1b96e4420ad039d315a6d455ed8b1768e500c
John Doe <johndoe@example.com>
2022-11-06 04:04:43 -0500

    workaround for upload errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.53',
+      version='0.54',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
