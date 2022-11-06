98d0624ab46cb625ff6330feb89fc582c1ebbde3
John Doe <johndoe@example.com>
2022-11-06 09:30:04 -0500

    exception fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.54',
+      version='0.55',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
