43392422643e73c88d90148570b8f25728cb7c05
John Doe <johndoe@example.com>
2023-11-10 14:02:32 -0500

    moving off of arkb, added progress output to presence check which is slow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='gitlake',
-      version='0.58',
+      version='0.59',
       description='Decentralized special remotes for git-annex',
       author='Karl Semich',
       author_email='0xloem@gmail.com',
